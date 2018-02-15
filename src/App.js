46541ff923e5d13776bbe8114440bb1c013c8a6c
nobuti <buti@nobuti.com>
2018-02-15 18:56:07 +0100

    Include Typography components in the root application.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
 import React, { Component } from 'react';
-import { Grid } from './components';
+import { Grid, Jumbo, Display, Title, Subheader, Text } from './components';
 
 class App extends Component {
   render() {
     return (
-      <Grid width={480} gap={20} align="center">
-        <div>Column</div>
-        <div>Column</div>
-        <div>Column</div>
-        <div>Column</div>
-        <div>Column</div>
-      </Grid>
+      <div>
+        <Grid width={480} gap={20} align="center">
+          <div>Column</div>
+          <div>Column</div>
+          <div>Column</div>
+          <div>Column</div>
+          <div>Column</div>
+        </Grid>
+
+        <Jumbo>I'm thinking two circus clowns dancing. You?</Jumbo>
+        <Jumbo as="h2">I'm a Jumbo h2</Jumbo>
+        <Jumbo as="h3" color="#ff9900">
+          I'm a Jumbo h3
+        </Jumbo>
+
+        <Display margin="0 0 20px">
+          I'm thinking two circus clowns dancing. You?
+        </Display>
+        <Display as="h2">I'm a Display h2</Display>
+        <Display as="h3" color="#ff9900">
+          I'm a Display h3
+        </Display>
+
+        <Title>I'm thinking two circus clowns dancing. You?</Title>
+        <Title as="h2">I'm a Title h2</Title>
+        <Title as="h3" color="#ff9900">
+          I'm a Title h3
+        </Title>
+
+        <Subheader>I'm thinking two circus clowns dancing. You?</Subheader>
+        <Subheader as="h2">I'm a Subheader h2</Subheader>
+        <Subheader as="h3" color="#ff9900">
+          I'm a Subheader h3
+        </Subheader>
+
+        <Text>I'm thinking two circus clowns dancing. You?</Text>
+        <Text as="div">I'm a Text div</Text>
+        <Text color="#ff9900">I'm a Text colored</Text>
+      </div>
     );
   }
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,6 @@
 export { default as Grid } from './Grid/grid';
+export { default as Jumbo } from './Typography/jumbo';
+export { default as Display } from './Typography/display';
+export { default as Title } from './Typography/title';
+export { default as Subheader } from './Typography/subheader';
+export { default as Text } from './Typography/text';
