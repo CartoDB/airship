f287a378f843059b4395d53131807848775c1c13
nobuti <buti@nobuti.com>
2018-02-16 11:11:34 +0100

    Try to generate documentation from separated folder.
    Not working, issue open, let's see.

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,16 +1,41 @@
 module.exports = {
-  skipComponentsWithoutExample: true,
   template: 'styleguide/template.html',
   title: 'Airship',
+  skipComponentsWithoutExample: true,
+  getExampleFilename(componentPath) {
+    return componentPath.replace(/(.+)\/(.+).js$/, '/docs/components/$2.md');
+  },
   sections: [
     {
       name: 'Introduction',
       content: './docs/introduction.md'
     },
     {
+      name: 'Typography',
+      content: './docs/typography.md',
+      components: function() {
+        return [
+          './src/components/Typography/jumbo.js',
+          './src/components/Typography/display.js',
+          './src/components/Typography/title.js',
+          './src/components/Typography/subheader.js',
+          './src/components/Typography/text.js',
+          './src/components/Typography/caption.js'
+        ];
+      },
+      getExampleFilename(componentPath) {
+        return componentPath.replace(
+          /(.+)\/(.+).js$/,
+          '/docs/components/$2.md'
+        );
+      }
+    },
+    {
       name: 'Components',
       content: './docs/components.md',
-      components: './src/components/**/*.js'
+      components: function() {
+        return ['./src/components/Grid/grid.js'];
+      }
     }
   ]
 };
