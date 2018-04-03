691b7aa016be0ae8bf5f5c03cee9e1d3951e5155
rubenmoya <rmoyarodriguez@gmail.com>
2018-04-03 20:17:49 +0200

    Remove Lodash

--- src/components/Typography/title.js
+++ src/components/Typography/title.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Base from './base';
-import _ from 'lodash';
 
 const Title = (props) => {
-  const { as } = props;
-  const extended = _.omit(props, 'font');
+  const { as, children, font, ...others } = props;
+
   const Text = Base.withComponent(as || 'h1').extend`
     font-size: 24px;
     line-height: 32px;
   `;
-  return <Text {...extended}>{props.children}</Text>;
+
+  return <Text {...others}>{children}</Text>;
 };
 
 export default Title;
