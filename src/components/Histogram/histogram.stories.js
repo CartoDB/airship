dbd5d07fcf82e6786028db0a83346bf5dd6b863a
rubenmoya <rmoyarodriguez@gmail.com>
2018-04-07 12:28:52 +0200

    Update histogram when data changes

--- src/components/Histogram/histogram.stories.js
+++ src/components/Histogram/histogram.stories.js
@@ -5,7 +5,26 @@ import Histogram from './histogram';
 import Widget from '../Widget/widget';
 import mockData from './histogram.fixtures';
 
-const DATA = mockData.bins.map(({ min, max, freq }) => ({ min, max, freq }))
+class HistogramUpdated extends React.Component {
+  state = {
+    data: mockData
+  }
+
+  changeState = () => {
+    this.setState(prevState => ({
+      data: prevState.data.length !== 4 ? mockData.slice(0, 4) : mockData
+    }))
+  }
+
+  render() {
+    return (
+      <div>
+        <Histogram data={this.state.data} />
+        <button onClick={this.changeState}>Click me</button>
+      </div>
+    )
+  }
+}
 
 storiesOf('Histogram', module)
   .add('Default', () => (
@@ -17,5 +36,13 @@ storiesOf('Histogram', module)
       <Widget.Description>Just a widget</Widget.Description>
 
       <Histogram data={mockData} />
+    </Widget>
+  ))
+  .add('Updating data', () => (
+    <Widget>
+      <Widget.Title>Suffer score</Widget.Title>
+      <Widget.Description>Just a widget</Widget.Description>
+
+      <HistogramUpdated />
     </Widget>
   ));
--- src/components/Histogram/histogramChart.js
+++ src/components/Histogram/histogramChart.js
@@ -2,6 +2,7 @@ import { select, selectAll } from 'd3-selection';
 import { scaleBand, scaleLinear } from 'd3-scale';
 import { rgb } from 'd3-color';
 import { min, max } from 'd3-array';
+import { transition } from 'd3-transition';
 import { axisBottom, axisLeft } from 'd3-axis';
 import { readableNumber, truncate } from '../../utils';
 
@@ -16,7 +17,7 @@ const DEFAULT_OPTIONS = {
   },
 };
 
-export default class Gaugechart {
+export default class HistogramChart {
   constructor(element, options) {
     if (!element) throw new Error('A root container is required');
 
@@ -27,12 +28,12 @@ export default class Gaugechart {
   }
 
   createChart() {
-    const { data: { bins }, height, margin, width } = this.options;
+    const { data, height, margin, width } = this.options;
     const fullHeight = height + margin.top + margin.bottom;
     const fullWidth = width + margin.left + margin.right;
 
     // -- Base SVG
-    const svg = select(this.element)
+    this.svg = select(this.element)
       .append('svg')
         .attr('width', fullWidth)
         .attr('height', fullHeight)
@@ -40,47 +41,81 @@ export default class Gaugechart {
         .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
     // -- X Axis
-    const xScale = scaleBand()
+    this.xScale = scaleBand()
       .paddingInner(0.05)
       .paddingOuter(0.1)
-      .domain(bins.map(d => d.max))
+      .domain(data.map(d => d.max))
       .range([0, width]);
 
     // -- Y Axis
-    const yScale = scaleLinear()
-      .domain([0, max(bins, d => d.freq)])
+    this.yScale = scaleLinear()
+      .domain([0, max(data, d => d.freq)])
       .range([height, 0]);
 
-    const yAxis = axisLeft(yScale)
+    this.yAxis = axisLeft(this.yScale)
       .tickSize(-width, 0, 0)
       .ticks(5);
 
-    svg.call(yAxis);
+    this.yAxisSelection = this.svg
+      .append('g')
+      .call(this.yAxis);
 
-    select('.domain').remove();
+    select('.domain').remove(); // Remove axis border
 
-    // -- Create bars
-    svg.selectAll('rect')
-      .data(bins)
-      .enter()
-      .append('rect')
-        .attr('class', 'bar')
-        .attr('x', d => xScale(d.max))
-        .attr('y', d => yScale(d.freq))
-        .attr('width', d => xScale.bandwidth())
-        .attr('height', d => yScale(0) - yScale(d.freq))
-
-
-    // this._refresh();
+    this.render();
   }
 
   update(options) {
     this.options = { ...this.options, ...options };
 
-    this.createChart(); // TODO: Replace with refresh and animations
+    this.render();
   }
 
-  _refresh() {
-    const { data, colors, showLegend } = this.options;
+  render() {
+    const { data, height, margin, width } = this.options;
+
+    // -- Create bars
+    const update = this.svg
+      .selectAll('rect')
+      .data(data)
+
+    update
+      .exit()
+      .attr('y', height)
+      .attr('height', 0)
+      .remove(); // Remove elements without data
+
+    this.yScale
+      .domain([0, max(data, d => d.freq)]);
+
+    this.xScale
+      .domain(data.map(d => d.max))
+
+    this.yAxisSelection
+      .call(this.yAxis);
+
+    select('.domain').remove(); // Remove axis border
+
+    update
+      .attr('class', 'bar')
+      .attr('y', height)
+      .attr('height', 0)
+      .attr('x', d => this.xScale(d.max))
+      .attr('width', d => this.xScale.bandwidth())
+      .attr('y', d => this.yScale(d.freq))
+      .attr('height', d => height - this.yScale(d.freq))
+
+    update
+      .enter()
+      .append('rect')
+        .attr('class', 'bar')
+        .attr('y', height)
+        .attr('height', 0)
+        .attr('x', d => this.xScale(d.max))
+        .attr('width', d => this.xScale.bandwidth())
+        .transition()
+          .delay(200)
+        .attr('y', d => this.yScale(d.freq))
+        .attr('height', d => height - this.yScale(d.freq))
   }
 }
