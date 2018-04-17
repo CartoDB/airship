4ebe3fae12ec7d91bdb0662b2d2c104399605824
rubenmoya <ruben@carto.com>
2018-04-17 17:01:04 +0200

    Add theme to Table

--- src/components/Table/table.js
+++ src/components/Table/table.js
@@ -1,72 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-
-/*
-  <Table>
-    <Table.Header>
-      <Table.Row>
-        <Table.HeaderCell>Header</Table.HeaderCell>
-        <Table.HeaderCell>Header</Table.HeaderCell>
-        <Table.HeaderCell>Header</Table.HeaderCell>
-      </Table.Row>
-    </Table.Header>
-
-    <Table.Body>
-      <Table.Row>
-        <Table.Cell>Cell</Table.Cell>
-        <Table.Cell>Cell</Table.Cell>
-        <Table.Cell>Cell</Table.Cell>
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>Cell</Table.Cell>
-        <Table.Cell>Cell</Table.Cell>
-        <Table.Cell>Cell</Table.Cell>
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>Cell</Table.Cell>
-        <Table.Cell>Cell</Table.Cell>
-        <Table.Cell>Cell</Table.Cell>
-      </Table.Row>
-    </Table.Body>
-  </Table>
-*/
-
-const StyledTable = styled.table`
-  border-collapse: collapse;
-  border-spacing: 0;
-  border: 1px solid #d1d5d7;
-  width: 100%;
-  font: 400 12px/20px 'Roboto';
-  color: #2c2c2c;
-
-  th {
-    font-weight: 600;
-    text-align: left;
-    background: #e2e6e3;
-  }
-
-  th,
-  td {
-    padding: 8px 12px;
-    border-bottom: 1px solid #d1d5d7;
-    border-right: ${props => (props.lined ? '1px solid #d1d5d7' : null)};
-  }
-
-  tr:last-child td {
-    border-bottom: 0;
-  }
-
-  tr:hover,
-  td:hover {
-    background: rgba(71, 219, 153, 0.16);
-  }
-
-  th:last-child,
-  td:last-child {
-    border-right: ${props => (props.lined ? 0 : null)};
-  }
-`;
+import { rgba } from 'polished';
+import { theme } from '../../constants';
 
 const Header = ({ children }) => <thead>{children}</thead>;
 
@@ -102,11 +38,41 @@ Cell.propTypes = {
   rowSpan: PropTypes.number,
 };
 
-const Table = ({ children, lined }) => (
-  <React.Fragment>
-    <StyledTable lined={lined}>{children}</StyledTable>
-  </React.Fragment>
-);
+const Table = styled.table`
+  border-collapse: collapse;
+  border-spacing: 0;
+  border: 1px solid ${props => props.theme.ui04};
+  width: 100%;
+  font: 400 12px/20px 'Roboto';
+  color: ${props => props.theme.type01};
+
+  th {
+    font-weight: 600;
+    text-align: left;
+    background: ${props => props.theme.ui03};
+  }
+
+  th,
+  td {
+    padding: 8px 12px;
+    border-bottom: 1px solid ${props => props.theme.ui04};
+    border-right: ${props => (props.lined ? `1px solid ${props.theme.ui04}` : null)};
+  }
+
+  tr:last-child td {
+    border-bottom: 0;
+  }
+
+  tr:hover,
+  td:hover {
+    background: ${props => rgba(props.theme.brand03, 0.16)};
+  }
+
+  th:last-child,
+  td:last-child {
+    border-right: ${props => (props.lined ? 0 : null)};
+  }
+`;
 
 Table.Header = Header;
 Table.HeaderCell = HeaderCell;
@@ -117,6 +83,10 @@ Table.Cell = Cell;
 Table.propTypes = {
   children: PropTypes.node,
   lined: PropTypes.bool,
+};
+
+Table.defaultProps = {
+  theme,
 };
 
 export default Table;
