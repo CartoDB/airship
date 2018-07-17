30e5d41a6d495e8cd57f48a5bb8f2b163d9d4372
jesusbotella <jesus.botella@gmail.com>
2018-07-17 11:34:00 +0200

    Add packages commands and global execution with LernaJS

--- packages/components/stencil.config.js
+++ packages/components/stencil.config.js
@@ -4,21 +4,10 @@ const sass = require('@stencil/sass');
 exports.config = {
   namespace: 'airship',
   outputTargets:[
-    {
-      type: 'dist'
-    },
-    {
-      type: 'www',
-      serviceWorker: false
-    }
+    { type: 'dist' },
+    { type: 'www', serviceWorker: false }
   ],
-  enableCache: false,
   plugins: [
     sass()
-  ],
-  includeSrc: [
-    '**/*.ts',
-    '**/*.tsx',
-    path.join(__dirname, '../', 'styles/src/**/*.scss')
   ]
 };
