edadd0933dc3c96013a5ac1cab7b6bb20b42f52d
jesusbotella <jesus.botella@gmail.com>
2018-07-17 19:14:37 +0200

    First test approach

--- packages/components/src/components/as-switch/as-switch.spec.ts
+++ packages/components/src/components/as-switch/as-switch.spec.ts
@@ -2,42 +2,33 @@ import { TestWindow } from '@stencil/core/testing';
 import { Switch } from './as-switch';
 
 describe('as-switch', () => {
+  let element: HTMLSwitchElement;
+  let testWindow: TestWindow;
+
+  beforeEach(async () => {
+    testWindow = new TestWindow();
+    element = await testWindow.load({
+      components: [Switch],
+      html: '<as-switch></as-switch>'
+    });
+  });
+
   it('should build', () => {
     expect(new Switch()).toBeTruthy();
   });
 
-  describe('rendering', () => {
-    let element: HTMLMyComponentElement;
-    let testWindow: TestWindow;
-    beforeEach(async () => {
-      testWindow = new TestWindow();
-      element = await testWindow.load({
-        components: [Switch],
-        html: '<as-switch></as-switch>'
-      });
-    });
+  describe('Rendering', () => {
 
-    it('should work without parameters', () => {
-      expect(element.textContent.trim()).toEqual('Hello, World! I\'m');
-    });
+  });
 
-    it('should work with a first name', async () => {
-      element.first = 'Peter';
+  describe('Behaviour', () => {
+    it('should toggle when the switch is clicked', async () => {
+      expect(element.querySelector('input').checked).toBeFalsy();
+      element.click();
       await testWindow.flush();
-      expect(element.textContent.trim()).toEqual('Hello, World! I\'m Peter');
-    });
 
-    it('should work with a last name', async () => {
-      element.last = 'Parker';
-      await testWindow.flush();
-      expect(element.textContent.trim()).toEqual('Hello, World! I\'m  Parker');
-    });
-
-    it('should work with both a first and a last name', async () => {
-      element.first = 'Peter';
-      element.last = 'Parker';
-      await testWindow.flush();
-      expect(element.textContent.trim()).toEqual('Hello, World! I\'m Peter Parker');
+      console.log(element.querySelector('input').checked);
+      expect(element.querySelector('input').checked).toBeTruthy();
     });
   });
 });
