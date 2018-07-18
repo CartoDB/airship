72c31d75bd7791dd9055a002fe106bf1d8b2584f
jesusbotella <jesus.botella@gmail.com>
2018-07-18 08:40:14 +0200

    First test passing

--- packages/components/src/components/as-switch/as-switch.spec.ts
+++ packages/components/src/components/as-switch/as-switch.spec.ts
@@ -2,33 +2,30 @@ import { TestWindow } from '@stencil/core/testing';
 import { Switch } from './as-switch';
 
 describe('as-switch', () => {
-  let element: HTMLSwitchElement;
-  let testWindow: TestWindow;
-
-  beforeEach(async () => {
-    testWindow = new TestWindow();
-    element = await testWindow.load({
-      components: [Switch],
-      html: '<as-switch></as-switch>'
-    });
-  });
-
   it('should build', () => {
     expect(new Switch()).toBeTruthy();
   });
 
-  describe('Rendering', () => {
-
-  });
-
   describe('Behaviour', () => {
-    it('should toggle when the switch is clicked', async () => {
-      expect(element.querySelector('input').checked).toBeFalsy();
-      element.click();
-      await testWindow.flush();
+    let element: HTMLSwitchElement;
+    let testWindow: TestWindow;
 
-      console.log(element.querySelector('input').checked);
-      expect(element.querySelector('input').checked).toBeTruthy();
+    beforeEach(async () => {
+      testWindow = new TestWindow();
+      element = await testWindow.load({
+        components: [Switch],
+        html: '<as-switch></as-switch>'
+      });
+    });
+
+    it('should emit an event with current status when component toggles', () => {
+      const spy = jest.fn();
+      element.addEventListener('onToggle', spy);
+
+      element.querySelector('input').click();
+
+      expect(spy).toHaveBeenCalled();
+      expect(spy.mock.calls[0][0].detail).toBe(true);
     });
   });
 });
