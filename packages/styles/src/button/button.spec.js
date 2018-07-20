7aa958e8bae48933db0c116c18754486877c6078
Víctor Velarde <victor@carto.com>
2018-07-20 12:31:35 +0200

    Add pattern to generated images

--- packages/styles/src/button/button.spec.js
+++ packages/styles/src/button/button.spec.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 const reference = path.resolve(__dirname, 'img/button-reference.png');
-const screenshot = path.resolve(__dirname, 'img/button.png');
+const screenshot = path.resolve(__dirname, 'img/button-out.png');
 
 // Url to take the screenshot from
 const url = `file://${path.resolve(__dirname, 'button.html')}`;
