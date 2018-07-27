37f763947442cd7a243120a82bb177c16a334ca4
jesusbotella <jesus.botella@gmail.com>
2018-07-27 16:21:34 +0200

    Panels test

---
+++ packages/styles/src/core/layout/panels/test/panels.spec.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+// Mobile Viewport Settings
+const viewportWidth = 375;
+const viewportHeight = 667;
+
+// Desktop
+const desktopReference = path.resolve(__dirname, 'img/panels-reference.png');
+const desktopScreenshot = path.resolve(__dirname, 'img/panels-out.png');
+const url = `file://${path.resolve(__dirname, 'panels.html')}`;
+
+// Mobile
+const mobileReference = path.resolve(__dirname, 'img/panels-mobile-reference.png');
+const mobileScreenshot = path.resolve(__dirname, 'img/panels-mobile-out.png');
+const url = `file://${path.resolve(__dirname, 'panels-mobile.html')}`;
+
+module.exports = [
+  { reference: desktopReference, screenshot: desktopScreenshot, url },
+  { reference: mobileReference, screenshot: mobileScreenshot, url, viewportWidth, viewportHeight },
+];
