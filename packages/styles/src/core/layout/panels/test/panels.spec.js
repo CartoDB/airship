4800159b7bd27926041b2353291db2208b9d7b34
jesusbotella <jesus.botella@gmail.com>
2018-07-27 16:35:10 +0200

    Fix panels test

--- packages/styles/src/core/layout/panels/test/panels.spec.js
+++ packages/styles/src/core/layout/panels/test/panels.spec.js
@@ -12,9 +12,9 @@ const url = `file://${path.resolve(__dirname, 'panels.html')}`;
 // Mobile
 const mobileReference = path.resolve(__dirname, 'img/panels-mobile-reference.png');
 const mobileScreenshot = path.resolve(__dirname, 'img/panels-mobile-out.png');
-const url = `file://${path.resolve(__dirname, 'panels-mobile.html')}`;
+const mobileURL = `file://${path.resolve(__dirname, 'panels-mobile.html')}`;
 
 module.exports = [
   { reference: desktopReference, screenshot: desktopScreenshot, url },
-  { reference: mobileReference, screenshot: mobileScreenshot, url, viewportWidth, viewportHeight },
+  { reference: mobileReference, screenshot: mobileScreenshot, url: mobileURL, viewportWidth, viewportHeight },
 ];
