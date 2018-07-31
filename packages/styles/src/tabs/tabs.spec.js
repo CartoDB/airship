4e5daadfa81ef334fdc396822b6c321e3bda7dbd
IagoLast <iago.lastra@gmail.com>
2018-07-31 11:50:53 +0200

    Fix test urls

--- packages/styles/src/tabs/tabs.spec.js
+++ packages/styles/src/tabs/tabs.spec.js
@@ -3,14 +3,14 @@ const path = require('path');
 
 
 // tabs
-const TABS_REFERENCE = path.resolve(__dirname, 'img/tabs-reference.png');
-const TABS_SCREENSHOT = path.resolve(__dirname, 'img/tabs-out.png');
-const TABS_URL = `file://${path.resolve(__dirname, 'tabs.html')}`;
+const TABS_REFERENCE = path.resolve(__dirname, 'test/img/tabs-reference.png');
+const TABS_SCREENSHOT = path.resolve(__dirname, 'test/img/tabs-out.png');
+const TABS_URL = `file://${path.resolve(__dirname, 'test/tabs.html')}`;
 
 // tabs-xl
-const TABS_XL_REFERENCE = path.resolve(__dirname, 'img/tabs-xl-reference.png');
-const TABS_XL_SCREENSHOT = path.resolve(__dirname, 'img/tabs-xl-out.png');
-const TABS_XL_URL = `file://${path.resolve(__dirname, 'tabs-xl.html')}`;
+const TABS_XL_REFERENCE = path.resolve(__dirname, 'test/img/tabs-xl-reference.png');
+const TABS_XL_SCREENSHOT = path.resolve(__dirname, 'test/img/tabs-xl-out.png');
+const TABS_XL_URL = `file://${path.resolve(__dirname, 'test/tabs-xl.html')}`;
 
 module.exports = [
   { reference: TABS_REFERENCE, screenshot: TABS_SCREENSHOT, url: TABS_URL },
