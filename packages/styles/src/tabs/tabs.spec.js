c36512683ca21f3193be88a6bd06fe7adf80ce0c
IagoLast <iago.lastra@gmail.com>
2018-07-31 11:10:05 +0200

    Add tabs test

--- packages/styles/src/tabs/tabs.spec.js
+++ packages/styles/src/tabs/tabs.spec.js
@@ -1,1 +1,18 @@
 // TODO: Add UI test
+const path = require('path');
+
+
+// tabs
+const TABS_REFERENCE = path.resolve(__dirname, 'img/tabs-reference.png');
+const TABS_SCREENSHOT = path.resolve(__dirname, 'img/tabs-out.png');
+const TABS_URL = `file://${path.resolve(__dirname, 'tabs.html')}`;
+
+// tabs-xl
+const TABS_XL_REFERENCE = path.resolve(__dirname, 'img/tabs-xl-reference.png');
+const TABS_XL_SCREENSHOT = path.resolve(__dirname, 'img/tabs-xl-out.png');
+const TABS_XL_URL = `file://${path.resolve(__dirname, 'tabs-xl.html')}`;
+
+module.exports = [
+  { reference: TABS_REFERENCE, screenshot: TABS_SCREENSHOT, url: TABS_URL },
+  { reference: TABS_XL_REFERENCE, screenshot: TABS_XL_SCREENSHOT, url: TABS_XL_URL },
+];
