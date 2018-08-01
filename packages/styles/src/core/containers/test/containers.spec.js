00a647f297f8360f07cc985ad1afb3463b5d68fc
Ivan Malagon <hacheka@gmail.com>
2018-08-01 13:11:16 +0200

    Complete specs

--- packages/styles/src/core/containers/test/containers.spec.js
+++ packages/styles/src/core/containers/test/containers.spec.js
@@ -38,8 +38,43 @@ const sidebarUrl = html('containers-in-sidebar.html');
 const sidebarMobileReference = image('containers-sidebar-mobile-reference.png');
 const sidebarMobileScreenshot = image('containers-sidebar-mobile-out.png');
 
+// Bottom bar
+// Desktop
+const bottomDesktopReference = image('containers-bottom-reference.png');
+const bottomDesktopScreenshot = image('containers-bottom-out.png');
+const bottomUrl = html('containers-in-bottom.html');
+
+// Mobile
+const bottomMobileReference = image('containers-bottom-mobile-reference.png');
+const bottomMobileScreenshot = image('containers-bottom-mobile-out.png');
+
+// Legends
+// Desktop
+const legendsDesktopReference = image('containers-legends-reference.png');
+const legendsDesktopScreenshot = image('containers-legends-out.png');
+const legendsUrl = html('containers-in-legends.html');
+
+// Mobile
+const legendsMobileReference = image('containers-legends-mobile-reference.png');
+const legendsMobileScreenshot = image('containers-legends-mobile-out.png');
+
+// All together
+// Desktop
+const allDesktopReference = image('containers-all-reference.png');
+const allDesktopScreenshot = image('containers-all-out.png');
+const allUrl = html('containers-in-sidebar-bottom-and-legends.html');
+
+// Mobile
+const allMobileReference = image('containers-all-mobile-reference.png');
+const allMobileScreenshot = image('containers-all-mobile-out.png');
+
 
 module.exports = [
   spec(sidebarDesktopReference, sidebarDesktopScreenshot, sidebarUrl),
-  spec(sidebarMobileReference, sidebarMobileScreenshot, sidebarUrl, true)
+  spec(sidebarMobileReference, sidebarMobileScreenshot, sidebarUrl, true),
+  spec(bottomDesktopReference, bottomDesktopScreenshot, bottomUrl),
+  spec(bottomMobileReference, bottomMobileScreenshot, bottomUrl, true)
+  spec(legendsDesktopReference, legendsDesktopScreenshot, legendsUrl),
+  spec(legendsMobileReference, legendsMobileScreenshot, legendsUrl, true),
+  spec(allDesktopReference, allDesktopScreenshot, allUrl)
 ];
