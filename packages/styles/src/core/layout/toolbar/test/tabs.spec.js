05c550331a5443b3dba4c0f7d423ca555e51a413
IagoLast <iago.lastra@gmail.com>
2018-08-03 22:14:50 +0200

    Update test suite

--- packages/styles/src/core/layout/toolbar/test/tabs.spec.js
+++ packages/styles/src/core/layout/toolbar/test/tabs.spec.js
@@ -1,20 +1,11 @@
 const Utils = require('../../../../test-utils');
 const u = new Utils(__dirname);
 
-// Desktop test
-// Tabs should be hidden
-const reference = u.image('layout.tabs-reference.png');
-const screenshot = u.image('layout.tabs-out.png');
-
-// Desktop test
-// Tabs should be hidden
-const mobileReference = u.image('layout.tabs-mobile-reference.png');
-const mobileScreenshot = u.image('layout.tabs-mobile-out.png');
-
-// URL to take the screenshot from
-const url = u.html('tabs.html');
-
 module.exports = [
-  u.spec(reference, screenshot, url),
-  u.spec(mobileReference, mobileScreenshot, url, true)
+  u.spec('tabs'),
+  u.spec('tabs', true),
+  u.spec('tabs-with-actions'),
+  u.spec('tabs-with-actions', true),
+  u.spec('tabs-with-actions-visible'),
+  u.spec('tabs-with-actions-visible', true),
 ];
--- packages/styles/src/tabs/test/tabs.spec.js
+++ packages/styles/src/tabs/test/tabs.spec.js
@@ -1,23 +1,11 @@
 const Utils = require('../../test-utils');
 const u = new Utils(__dirname);
 
-// tabs
-const TABS_REFERENCE = u.image('tabs-reference.png');
-const TABS_SCREENSHOT = u.image('tabs-out.png');
-const TABS_URL = u.html('tabs.html');
-
-// tabs-xl
-const TABS_XL_REFERENCE = u.image('tabs-xl-reference.png');
-const TABS_XL_SCREENSHOT = u.image('tabs-xl-out.png');
-const TABS_XL_URL = u.html('tabs-xl.html');
-
-// tabs-accesible
-const TABS_ACCESIBLE_REFERENCE = u.image('tabs-accesible-reference.png');
-const TABS_ACCESIBLE_SCREENSHOT = u.image('tabs-accesible-out.png');
-const TABS_ACCESIBLE_URL = u.html('tabs-accesible.html');
-
 module.exports = [
-  u.spec(TABS_REFERENCE, TABS_SCREENSHOT, TABS_URL),
-  u.spec(TABS_XL_REFERENCE, TABS_XL_SCREENSHOT, TABS_XL_URL),
-  u.spec(TABS_ACCESIBLE_REFERENCE, TABS_ACCESIBLE_SCREENSHOT, TABS_ACCESIBLE_URL)
+  u.spec('tabs'),
+  u.spec('tabs', true),
+  u.spec('tabs-xl'),
+  u.spec('tabs-xl', true),
+  u.spec('tabs-accesible'),
+  u.spec('tabs-accesible', true)
 ];
