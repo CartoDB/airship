8e186bc20d6806027029d657f81b51a354c4e650
IagoLast <iago.lastra@gmail.com>
2018-08-03 18:43:44 +0200

    Add UI test

--- packages/styles/src/buttons/buttons.spec.js
+++ packages/styles/src/buttons/buttons.spec.js
@@ -21,9 +21,15 @@ const BUTTON_ELEMENTS_REFERENCE = path.resolve(__dirname, 'test/img/button-eleme
 const BUTTON_ELEMENTS_SCREENSHOT = path.resolve(__dirname, 'test/img/button-elements-out.png');
 const BUTTON_ELEMENTS_URL = `file://${path.resolve(__dirname, 'test/button-elements.html')}`;
 
+// icons
+const BUTTON_ICONS_REFERENCE = path.resolve(__dirname, 'test/img/button-icons-reference.png');
+const BUTTON_ICONS_SCREENSHOT = path.resolve(__dirname, 'test/img/button-icons-out.png');
+const BUTTON_ICONS_URL = `file://${path.resolve(__dirname, 'test/button-icons.html')}`;
+
 module.exports = [
   { reference: BUTTON_BASE_REFERENCE, screenshot: BUTTON_BASE_SCREENSHOT, url: BUTTON_BASE_URL },
   { reference: BUTTON_PRIMARY_REFERENCE, screenshot: BUTTON_PRIMARY_SCREENSHOT, url: BUTTON_PRIMARY_URL },
   { reference: BUTTON_SECONDARY_REFERENCE, screenshot: BUTTON_SECONDARY_SCREENSHOT, url: BUTTON_SECONDARY_URL },
   { reference: BUTTON_ELEMENTS_REFERENCE, screenshot: BUTTON_ELEMENTS_SCREENSHOT, url: BUTTON_ELEMENTS_URL },
+  { reference: BUTTON_ICONS_REFERENCE, screenshot: BUTTON_ICONS_SCREENSHOT, url: BUTTON_ICONS_URL },
 ];
