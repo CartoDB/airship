7c61fc0b57971f7ca5cef9b74b67063b5be773e8
IagoLast <iago.lastra@gmail.com>
2018-08-06 09:24:14 +0200

    Add screenshot references

---
+++ packages/styles/src/flag/test/flag.spec.js
@@ -0,0 +1,9 @@
+const Utils = require('../../test-utils');
+const u = new Utils(__dirname);
+
+module.exports = [
+  u.spec('flag'),
+  u.spec('flag-block'),
+  u.spec('flag-l'),
+  u.spec('flag-xl'),
+];
---
+++ packages/styles/src/inputs/test/input.spec.js
@@ -0,0 +1,9 @@
+const Utils = require('../../test-utils');
+const u = new Utils(__dirname);
+
+module.exports = [
+  u.spec('input-base'),
+  u.spec('input-disabled'),
+  u.spec('input-invalid'),
+  u.spec('input-readonly'),
+];
