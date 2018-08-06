ecc29df3ded78a0fd77dfcb33f0f076f75fd1ce3
Ivan Malagon <hacheka@gmail.com>
2018-08-06 15:18:50 +0200

    Refactor specs

--- packages/styles/src/tabs/test/tabs.spec.js
+++ packages/styles/src/tabs/test/tabs.spec.js
@@ -1,23 +1,8 @@
 const Utils = require('../../test-utils');
 const u = new Utils(__dirname);
 
-// tabs
-const TABS_REFERENCE = u.image('tabs-reference.png');
-const TABS_SCREENSHOT = u.image('tabs-out.png');
-const TABS_URL = u.html('tabs.html');
-
-// tabs-xl
-const TABS_XL_REFERENCE = u.image('tabs-xl-reference.png');
-const TABS_XL_SCREENSHOT = u.image('tabs-xl-out.png');
-const TABS_XL_URL = u.html('tabs-xl.html');
-
-// tabs-accesible
-const TABS_ACCESIBLE_REFERENCE = u.image('tabs-accesible-reference.png');
-const TABS_ACCESIBLE_SCREENSHOT = u.image('tabs-accesible-out.png');
-const TABS_ACCESIBLE_URL = u.html('tabs-accesible.html');
-
 module.exports = [
-  u.spec(TABS_REFERENCE, TABS_SCREENSHOT, TABS_URL),
-  u.spec(TABS_XL_REFERENCE, TABS_XL_SCREENSHOT, TABS_XL_URL),
-  u.spec(TABS_ACCESIBLE_REFERENCE, TABS_ACCESIBLE_SCREENSHOT, TABS_ACCESIBLE_URL)
+  u.spec('Tabs', 'tabs.html'),
+  u.spec('Tabs XL', 'tabs-xl.html'),
+  u.spec('Tabs accesible', 'tabs-accesible.html')
 ];
