242a74ccce51c6eec9998800559bab63bb1d3e77
rubenmoya <ruben@carto.com>
2018-08-21 11:46:50 +0200

    Add specs for infowindow

--- packages/components/src/components/as-infowindow/as-infowindow.spec.ts
+++ packages/components/src/components/as-infowindow/as-infowindow.spec.ts
@@ -1,7 +1,57 @@
+import { TestWindow } from '@stencil/core/dist/testing';
 import { Infowindow } from './as-infowindow';
 
-describe('as-switch', () => {
+describe('as-infowindow', () => {
   it('should build', () => {
     expect(new Infowindow()).toBeTruthy();
+  });
+
+  describe('Rendering', () => {
+    let element: HTMLAsCategoryWidgetElement;
+    let testWindow: TestWindow;
+
+    beforeEach(async () => {
+
+    });
+
+    it('should render content', async () => {
+      testWindow = new TestWindow();
+      element = await testWindow.load({
+        components: [Infowindow],
+        html: `
+          <as-infowindow>
+            Hello there!
+          </as-infowindow>
+        `
+      });
+
+      expect(element).toMatchSnapshot();
+    });
+
+    it('should render content and image', async () => {
+      testWindow = new TestWindow();
+      element = await testWindow.load({
+        components: [Infowindow],
+        html: `
+          <as-infowindow src="general_kenobi.jpg">
+            Hello there!
+          </as-infowindow>
+        `
+      });
+
+      expect(element).toMatchSnapshot();
+    });
+
+    it('should render with only an image', async () => {
+      testWindow = new TestWindow();
+      element = await testWindow.load({
+        components: [Infowindow],
+        html: `
+          <as-infowindow src="general_kenobi.jpg"></as-infowindow>
+        `
+      });
+
+      expect(element).toMatchSnapshot();
+    });
   });
 });
