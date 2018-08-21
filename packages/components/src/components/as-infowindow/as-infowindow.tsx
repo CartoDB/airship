882ef887f9372a9d3afa48449f72463b927ec74c
rubenmoya <ruben@carto.com>
2018-08-21 10:44:57 +0200

    Improve infowindow

--- packages/components/src/components/as-infowindow/as-infowindow.tsx
+++ packages/components/src/components/as-infowindow/as-infowindow.tsx
@@ -12,10 +12,9 @@ export class Infowindow {
   @Element() private element: HTMLElement;
 
   public render() {
-    if (this.src) {
-      return this._renderImageInfoWindow();
-    }
-    return this._renderStandarInfowindow();
+    return !this.element.innerHTML && this.src
+      ? this._renderImageInfoWindow()
+      : this._renderStandarInfowindow();
   }
 
   public componentDidLoad() {
@@ -27,6 +26,7 @@ export class Infowindow {
   private _renderStandarInfowindow() {
     return (
       <div class='as-infowindow'>
+        {this.src && <img src={this.src} />}
         <div class='as-infowindow__content'>
           <slot />
         </div>
@@ -39,10 +39,12 @@ export class Infowindow {
     return (
       <div class='as-infowindow'>
         <div class='as-infowindow__media'>
-          <img src={this.src} class='as-infowindow-media-item' />
-        </div>
-        <div class='as-infowindow__hook--img'>
           <img src={this.src} />
+        </div>
+        <div class='as-infowindow__imageHook'>
+          <div class='as-infowindow__imageHook--inner'>
+            <img src={this.src} />
+          </div>
         </div>
       </div>
     );
@@ -50,40 +52,21 @@ export class Infowindow {
 
   private _setupHook() {
     const imageElement = this.element.querySelector('.as-infowindow__media img') as HTMLImageElement;
-    imageElement.onload = () => {
-      const imageHeight = imageElement.height;
 
-      const imageWidth = imageElement.width;
-      const ratio = (imageHeight / imageWidth) * 20;
+    if (!imageElement) {
+      return;
+    }
+
+    imageElement.onload = () => {
+      const imageHeight = imageElement.offsetHeight;
 
       const media = this.element.querySelector('.as-infowindow__media') as HTMLDivElement;
-      const hook = this.element.querySelector('.as-infowindow__hook--img') as HTMLDivElement;
+      const hook = this.element.querySelector('.as-infowindow__imageHook img') as HTMLDivElement;
 
+      const offset = imageHeight - 18;
 
-      const newHeight = imageHeight - 10;
-      media.style.height = `${newHeight}px`;
-
-
-      hook.style.clipPath = this._computeClipPath(ratio);
+      media.style.height = `${offset}px`;
+      hook.style.marginTop = `-${offset}px`;
     };
-
-  }
-
-  /**
-   * Compute the hook form factor depending on the aspect ratio of the image.
-   * (Values are totally empirical)
-   */
-  private _computeClipPath(ratio: number): string {
-    if (ratio < 10) {
-      return 'polygon(0 0, 0% 100%, 40% 0%)';
-    }
-
-    if (ratio < 30) {
-      return 'polygon(0 0, 0% 100%, 100% 0%)';
-    }
-
-    return 'polygon(0 0, 0% 100%, 100% 60%)';
-
-
   }
 }
