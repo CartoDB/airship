d178d1585f388f135cdc4ae5efe24914d098b6e4
Ivan Malagon <hacheka@gmail.com>
2018-08-24 11:03:55 +0200

    Update references

--- packages/styles/src/core/layout/toolbar/test/with-actions.spec.js
+++ packages/styles/src/core/layout/toolbar/test/with-actions.spec.js
@@ -3,5 +3,6 @@ const u = new Utils(__dirname);
 
 module.exports = [
   u.spec('With actions', 'tabs-with-actions'),
-  u.spec('With actions mobile', 'tabs-with-actions-visible')
+  u.spec('With actions mobile', 'tabs-with-actions-visible'),
+  u.spec('With actions mobile left', 'tabs-with-actions-visible-left')
 ];
