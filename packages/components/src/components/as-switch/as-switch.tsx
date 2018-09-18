dcd278a2e3b7dd32e894a24e7020c475c2606079
IagoLast <iago.lastra@gmail.com>
2018-09-18 17:38:19 +0200

    ignore linter in long line

--- packages/components/src/components/as-switch/as-switch.tsx
+++ packages/components/src/components/as-switch/as-switch.tsx
@@ -80,6 +80,7 @@ export class Switch {
       (<svg class='as-switch__thumb' width='10' height='8' xmlns='http://www.w3.org/2000/svg'>
         <path
           fill='currentColor'
+          // tslint:disable-next-line
           d='M3.315 7.858L.133 4.441a.506.506 0 0 1 0-.683l.643-.684a.437.437 0 0 1 .642 0l2.219 2.393L8.58.141a.437.437 0 0 1 .643 0l.643.683a.504.504 0 0 1 0 .683l-5.91 6.35a.437.437 0 0 1-.642 0' />
       </svg>)
     ];
