cabcf6fdf22a3f205484d5aba0652c03f43180b9
IagoLast <iago.lastra@gmail.com>
2018-10-05 12:38:32 +0200

    Prevent .min.min.min when releasing a new version

--- packages/styles/scripts/rename-css-files.sh
+++ packages/styles/scripts/rename-css-files.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 rm -rf dist/**/*.min.css
-find dist/ -type f -name '*.css' -exec bash -c 'mv "$1" "${1%.css}".min.css' - '{}' \;
+find dist/ -type f -name '*.css' ! -name '*.min.css' -exec bash -c 'mv "$1" "${1%.css}".min.css' - '{}' \;
