591fe495e815bc84f17f09f383dd57aa6a7980c7
IagoLast <iago.lastra@gmail.com>
2018-10-08 15:15:59 +0200

    Pass tests

--- packages/components/src/components/as-stacked-bar-widget/data-processor.spec.js
+++ packages/components/src/components/as-stacked-bar-widget/data-processor.spec.js
@@ -1,10 +1,29 @@
 import dataProcessor from './data-processor';
 
 describe('data-processor', () => {
+  let FAKE_DATA;
+  beforeEach(() => {
+    FAKE_DATA = [{
+        category: 'A New Hope',
+        values: {
+          investment: 204338075,
+          revenue: 359029623
+        }
+      },
+      {
+        category: 'The Empire Strikes Back',
+        values: {
+          investment: 369029623,
+          revenue: 236513856
+        }
+      }
+    ];
+  });
+
   describe('.getDomain', () => {
     it('should return the biggest value in the data as the higher bound ', () => {
       const actual = dataProcessor.getDomain(FAKE_DATA)[1]
-      const expected = 359029623;
+      const expected = 369029623;
 
       expect(actual).toEqual(expected);
     });
@@ -31,23 +50,3 @@ describe('data-processor', () => {
     });
   });
 });
-
-
-
-
-
-const FAKE_DATA = [{
-    category: 'A New Hope',
-    values: {
-      investment: 204338075,
-      revenue: 359029623
-    }
-  },
-  {
-    category: 'The Empire Strikes Back',
-    values: {
-      investment: 369029623,
-      revenue: 236513856
-    }
-  }
-];
--- packages/components/src/components/as-stacked-bar-widget/data-processor.ts
+++ packages/components/src/components/as-stacked-bar-widget/data-processor.ts
@@ -1,8 +1,18 @@
 import { StackedbarData } from './as-stacked-bar-widget';
 
 export function getDomain(data: StackedbarData[]): number[] {
-  return data.reduce((previousValue: number[], currentValue: StackedbarData) => {
-    return previousValue;
+  return data.reduce((domain: number[], currentValue: StackedbarData) => {
+    for (const key of Object.keys(currentValue.values)) {
+      const value = currentValue.values[key];
+      if (value <= domain[0]) {
+        domain[0] = value;
+      }
+      if (value >= domain[1]) {
+        domain[1] = value;
+      }
+    }
+
+    return domain;
   }, [0, 0]);
 }
 
