935b7d7c170d20f36ff1ac504b10c11e97d8fcd0
IagoLast <iago.lastra@gmail.com>
2018-10-08 15:05:44 +0200

    Return [0,0] as default domain

--- packages/components/src/components/as-stacked-bar-widget/data-processor.ts
+++ packages/components/src/components/as-stacked-bar-widget/data-processor.ts
@@ -1,5 +1,9 @@
-export function getDomain(data) {
-  return data;
+import { StackedbarData } from './as-stacked-bar-widget';
+
+export function getDomain(data: StackedbarData[]): number[] {
+  return data.reduce((previousValue: number[], currentValue: StackedbarData) => {
+    return previousValue;
+  }, [0, 0]);
 }
 
 
