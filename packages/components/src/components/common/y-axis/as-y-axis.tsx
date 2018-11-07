5a3a3f8c63aa25bd08ff72ea6ed2c7d7e833b8ed
IagoLast <iago.lastra@gmail.com>
2018-11-07 11:46:55 +0100

    Refactor yAxis to use the service in render funcion

--- packages/components/src/components/common/y-axis/as-y-axis.tsx
+++ packages/components/src/components/common/y-axis/as-y-axis.tsx
@@ -1,8 +1,5 @@
 import { Component, Element, Prop } from '@stencil/core';
-import { axisLeft } from 'd3-axis';
-import { scaleLinear } from 'd3-scale';
-import { select, Selection } from 'd3-selection';
-import readableNumber from '../../../utils/readable-number';
+import yAxisService from './y-axis.service';
 
 
 /**
@@ -59,39 +56,10 @@ export class YAxis {
   }
 
   public render() {
-    const VERTICAL_SPACING = 36; // Need 16px top and bottom to view the labels
-    const TICK_RIGHT_MARGIN = 18;
-    const LABEL_WIDTH = 25;
-    const ELEMENT = select(this.element.previousElementSibling);
-    const HEIGHT = ELEMENT.node().getBoundingClientRect().height - VERTICAL_SPACING;
-    const WIDTH = ELEMENT.node().getBoundingClientRect().width;
-    const TICK_SIZE = - WIDTH + LABEL_WIDTH;
-    const RANGE = [HEIGHT, 0];
+    const element = this.element.previousElementSibling as SVGElement;
+    const scale = [this.from, this.to] as [number, number];
 
-    const yScale = scaleLinear()
-      .domain([this.from, this.to])
-      .range(RANGE);
-
-    const yAxis = axisLeft(yScale)
-      .tickSizeInner(TICK_SIZE + TICK_RIGHT_MARGIN)
-      .ticks(6)
-      .tickFormat((d) => `${readableNumber(d)}`);
-
-
-    if (ELEMENT.select('.y-axis').empty()) {
-      this._createYAxisElement(ELEMENT).call(yAxis);
-    } else {
-      ELEMENT.select('.y-axis').call(yAxis);
-    }
-
-    ELEMENT.selectAll('.tick text')
-      .attr('textLength', LABEL_WIDTH)
-      .attr('lengthAdjust', 'spacing');
-
-  }
-
-  private _createYAxisElement(element: Selection<Element, {}, null, undefined>) {
-    return element.append('g').attr('class', 'y-axis');
+    yAxisService.renderYAxis(element, scale);
   }
 
   private _resizeListener() {
