1aa2ecf5908dbb0940cc245ac4f7b26d166f3863
IagoLast <iago.lastra@gmail.com>
2018-11-16 18:49:27 +0100

    fix(components): Refactor as-responsive-content (#507)
    - Fix #503
    
    Co-Authored-By: IagoLast <iago.lastra@gmail.com>

--- packages/components/src/components/as-responsive-content/as-responsive-content.e2e.ts
+++ packages/components/src/components/as-responsive-content/as-responsive-content.e2e.ts
@@ -17,58 +17,41 @@ describe('as-responsive-content', () => {
       expect(tabsHTML).toContain('Panel 0');
       expect(tabsHTML).toContain('Bottom Bar');
     });
-
-    it('should render child content inside .as-content', async () => {
-      /* tslint:disable */
-      // DOM Example with spaces and line breaks removed
-      const innerContent = '<aside class=\"as-sidebar as-sidebar--left\">Left Sidebar</aside><main class=\"as-main\"><div class=\"as-map-area\"><div id=\"map\"></div><div class=\"as-map-panels\"><div class=\"as-panel as-panel--top as-panel--right\"><div class=\"as-panel__element\">Floating Panel</div></div></div></div><div class=\"as-map-footer\">Bottom Bar</div></main><aside class=\"as-sidebar as-sidebar--right\">Right Sidebar</aside>';
-      /* tslint:enable */
-
-      const asAppContentNode = await page.find('.as-content');
-      expect(asAppContentNode.innerHTML).toContain(innerContent);
-    });
   });
 
   describe('Behaviour', async () => {
-    it('should change active content when clicking tabs', async () => {
-      const tabs: E2EElement[] = await page.findAll('.as-tabs__item');
+    let tabs: E2EElement[];
+    beforeEach(async () => {
+      tabs = await page.findAll('.as-tabs__item');
+    });
+    it('should display the map-area by default', async () => {
+      const mapAreaElement = await page.find('.as-map-area');
+      expect(mapAreaElement).toHaveClass('as-map-area--visible');
+    });
 
-      let mapTab = tabs[0];
-      expect(mapTab).toHaveClasses(['as-tabs__item', 'as-tabs__item--active']);
-
-      let leftSidebarTab = tabs[1];
-      leftSidebarTab.click();
+    it('should activate a sidebar when clicking on its tab', async () => {
+      const leftSidebar = await page.find('.as-sidebar--left');
+      tabs[1].click();
 
       await page.waitForChanges();
-
-      const tabsAfterClick = await page.findAll('.as-tabs__item');
-      const leftSidebarNode = await page.find('.as-sidebar.as-sidebar--left');
-
-      mapTab = tabsAfterClick[0];
-      leftSidebarTab = tabsAfterClick[1];
-
-      expect(mapTab).toHaveClass('as-tabs__item');
-      expect(leftSidebarTab).toHaveClasses(['as-tabs__item', 'as-tabs__item--active']);
-      expect(leftSidebarNode).toHaveClasses(['as-sidebar', 'as-sidebar--left', 'as-sidebar--visible']);
+      expect(leftSidebar).toHaveClass('as-sidebar--visible');
     });
-  });
 
-  describe('Events', async () => {
-    // TODO: Fix this break tests
-    // it('should emit a "sectionChange" event when content changes in mobile view', async () => {
-    //   const sectionChangedSpy = await page.spyOnEvent('sectionChange');
-    //   await page.waitForChanges();
+    it('should activate map panels when clicking on its tab', async () => {
+      const panels = await page.find('.as-map-panels');
+      tabs[3].click();
 
-    //   const tabs: E2EElement[] = await page.findAll('.as-tabs__item');
-    //   tabs.forEach((tab) => console.log(tab.outerHTML));
+      await page.waitForChanges();
+      expect(panels).toHaveClass('as-map-panels--visible');
+    });
 
-    //   const leftSidebarTab = tabs[1];
-    //   await leftSidebarTab.tap();
+    it('should activate the footer when clicking on its tab', async () => {
+      const footer = await page.find('.as-map-footer');
+      tabs[4].click();
 
-    //   await page.waitForChanges();
-
-    //   // expect(sectionChangedSpy).toHaveReceivedEventDetail({});
-    // });
+      await page.waitForChanges();
+      expect(footer).toHaveClass('as-map-footer--visible');
+    });
   });
 });
 
