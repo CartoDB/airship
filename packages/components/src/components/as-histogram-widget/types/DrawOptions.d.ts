2192fa42f151b2b1f4b85089d90b5aa8927b805c
Román Jiménez <roman@cartodb.com>
2018-11-29 19:56:04 +0100

    fix linter

--- packages/components/src/components/as-histogram-widget/types/DrawOptions.d.ts
+++ packages/components/src/components/as-histogram-widget/types/DrawOptions.d.ts
@@ -1,12 +1,12 @@
-import { SVGContainer } from './Container';
 import { ScaleLinear } from 'd3';
+import { SVGContainer } from './Container';
 
 interface DrawOptions {
   container: SVGContainer;
   width: number;
-  handleWidth: number,
+  handleWidth: number;
   height: number;
   padding: [number, number];
-  xScale: ScaleLinear<number, number>,
-  binsScale: ScaleLinear<number, number>
+  xScale: ScaleLinear<number, number>;
+  binsScale: ScaleLinear<number, number>;
 }
