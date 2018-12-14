28cef2c6d0903b552cd07561b2126871d9ed98f5
Román Jiménez <roman@cartodb.com>
2018-12-14 17:03:59 +0100

    Improve S3 Upload
    Build regular bindings as well as minified ones

--- scripts/release-s3.js
+++ scripts/release-s3.js
@@ -50,34 +50,42 @@ function log (what) {
   console.log(what);
 }
 
-async function putObject (args) {
+async function putObject (args, dry = false) {
   return new Promise((resolve, reject) => {
-    S3.putObject(args, function (err, data) {
-      if (err) {
-        reject(err);
-        return;
-      }
-
-      resolve(data);
-    });
+    if (dry) {
+      resolve();
+    } else {
+      S3.putObject(args, function (err, data) {
+        if (err) {
+          reject(err);
+          return;
+        }
+  
+        resolve(data);
+      });
+    }
   });
 }
 
-async function copyObject (source, destination) {
+async function copyObject (source, destination, dry = false) {
   return new Promise((resolve, reject) => {
-    S3.copyObject({
-      Bucket: secrets.AWS_S3_BUCKET,
-      ACL: 'public-read',
-      CopySource: `${secrets.AWS_S3_BUCKET}/${source}`,
-      Key: destination
-    }, (err, data) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-
-      resolve(data);
-    });
+    if (dry) {
+      resolve();
+    } else {
+      S3.copyObject({
+        Bucket: secrets.AWS_S3_BUCKET,
+        ACL: 'public-read',
+        CopySource: `${secrets.AWS_S3_BUCKET}/${source}`,
+        Key: destination
+      }, (err, data) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+  
+        resolve(data);
+      });
+    }
   });
 }
 
@@ -92,6 +100,47 @@ async function listObjects (args) {
       resolve(data);
     });
   });
+}
+
+// For debugging
+async function listFiles (prefix = 'airship-style/') {
+  const files = await listObjects({Prefix: prefix, Bucket: secrets.AWS_S3_BUCKET });
+  log(`${files.Contents.length} with prefix '${prefix}'`)
+  for (let file of files.Contents) {
+    log(`${file.Key}`)
+  }
+}
+
+function parseFiles(files) {
+  return files.map((file) => ({
+    name: path.basename(file),
+    dir: path.dirname(file)
+  }));
+}
+
+/**
+ * Recursively walks through directory structure, returns all files as { name, dir }
+ * @param {string} fd Base directory to start walking
+ */
+async function getAllFiles(fd) {
+  if (fs.lstatSync(fd).isDirectory()) {
+    const fileList = await readdir(fd);
+    let files = [];
+
+    for (let file of fileList) {
+      const filePath = path.join(fd, file);
+
+      const subFiles = await getAllFiles(filePath)
+      files = files.concat(subFiles);
+    }
+
+    return files;
+  } else {
+    return [{
+      name: path.basename(fd),
+      dir: path.dirname(fd)
+    }];
+  }
 }
 
 const UPLOAD = [
@@ -112,18 +161,20 @@ const UPLOAD = [
     src: path.join(__dirname, '../packages/styles/dist'),
     dst: 'airship-style',
     version: require('../packages/styles/package.json').version
+  },
+  {
+    name: 'Airship Bindings',
+    src: [
+      path.join(__dirname, '../packages/bindings/dist/asbindings.js'),
+      path.join(__dirname, '../packages/bindings/dist/asbindings.min.js')
+    ],
+    dst: 'airship-bindings',
+    version: require('../packages/bindings/package.json').version
   }
 ];
 
-async function uploadAllFiles (dir, version, destination, subfolder='') {
+async function uploadAllFiles (files, version, destination) {
   const parsedVersion = semver(version);
-  let files;
-  try {
-    files = await readdir(dir);
-  } catch (e) {
-    console.error(e);
-    return;
-  }
 
   // All files will be uploaded to /version/, and copied to each of the versions here
   let copyVersions = [];
@@ -137,70 +188,65 @@ async function uploadAllFiles (dir, version, destination, subfolder='') {
     copyVersions.push(PRERELEASE_VERSION);
   }
 
-  for (const fileName of files) {
-    const filePath = path.join(dir, fileName);
-
-    if (fs.lstatSync(filePath).isDirectory()) {
-      await uploadAllFiles(filePath, version, destination, fileName);
-    } else {
-      const dst = path.join(subfolder, fileName);
-      const extension = path.extname(filePath);
-      const objectConfig = {
-        ACL: 'public-read',
-        Bucket: secrets.AWS_S3_BUCKET
-      };
-      let fileLength = 0; // uncompressed size
-      let fileContent;
-      let ratio = 0;
-
-      objectConfig.ContentType = `${mime.getType(filePath)}${needsUTF8(extension) ? '; charset=utf-8' : ''}`;
-
-      try {
-        fileContent = await readFile(filePath, needsUTF8(extension) ? { encoding: 'utf8' } : {});
-        fileLength = fileContent.length;
-
-        if (shouldZip(extension)) {
-          fileContent = await gzip(fileContent);
-          objectConfig.ContentEncoding = 'gzip';
-        }
-
-        objectConfig.Body = fileContent;
-
-        ratio = Math.round((fileContent.length / fileLength) * 100);
-      } catch (e) {
-        console.error(e);
-        continue;
-      }
-
-      objectConfig.Key = `${destination}/v${version}/${dst}`;
-
-      try {
-        spinner.setMessage(`Uploading ${objectConfig.Key}`);
-        if (!DRY_RUN) {
-          await putObject(objectConfig);
-        }
-
-        log(`✅  ${objectConfig.Key} ${fileContent.length} bytes. Compressed ${ratio > 100 ? `⚠️  \x1b[33m${ratio}` : ratio}%\x1b[0m`);
-      } catch (e) {
-        console.error(`❌  ${dst}`, e);
-        continue;
-      }
-
-      for (copyVersion of copyVersions) {
-        const dest = `${destination}/${copyVersion}/${dst}`;
-        spinner.setMessage(`Copying ${dest}`);
-        try {
-          if (!DRY_RUN) {
-            await copyObject(objectConfig.Key, dest);
-          }
-          log(`  ✅  Copied to ${dest}`);
-        } catch (e) {
-          console.error(`  ❌  Failed to copy to ${dest}`);
-          console.error(e);
-        }
-      }
-    }
+  for (const file of files) {
+    uploadFile(file, destination, copyVersions);
   }
+}
+
+async function uploadFile ({ dir, name}, destination, copyVersions) {
+  const filePath = path.join(dir, name);
+
+  const extension = path.extname(filePath);
+  const objectConfig = {
+    ACL: 'public-read',
+    Bucket: secrets.AWS_S3_BUCKET
+  };
+  let fileLength = 0; // uncompressed size
+  let fileContent;
+  let ratio = 0;
+
+  objectConfig.ContentType = `${mime.getType(filePath)}${needsUTF8(extension) ? '; charset=utf-8' : ''}`;
+
+  try {
+    fileContent = await readFile(filePath, needsUTF8(extension) ? { encoding: 'utf8' } : {});
+    fileLength = fileContent.length;
+
+    if (shouldZip(extension)) {
+      fileContent = await gzip(fileContent);
+      objectConfig.ContentEncoding = 'gzip';
+    }
+
+    objectConfig.Body = fileContent;
+
+    ratio = Math.round((fileContent.length / fileLength) * 100);
+  } catch (e) {
+    console.error(e);
+    return;
+  }
+
+  objectConfig.Key = `${destination}/v${version}/${name}`;
+
+  putObject(objectConfig, DRY_RUN)
+    .then(() => {
+      log(`✅  ${objectConfig.Key} ${fileContent.length} bytes. Compressed ${ratio > 100 ? `⚠️  \x1b[33m${ratio}` : ratio}%\x1b[0m`);
+    })
+    .then(() => {
+      for (copyVersion of copyVersions) {
+        const dest = `${destination}/${copyVersion}/${name}`;
+        spinner.setMessage(`Copying ${dest}`);
+        copyObject(objectConfig.Key, dest, DRY_RUN)
+          .then(() => {
+            log(`  ✅  Copied to ${dest}`);
+          })
+          .catch((e) => {
+            console.error(`  ❌  Failed to copy to ${dest}`);
+            console.error(e);
+          });
+      }
+    })
+    .catch((e) => {
+      console.error(`❌  ${name}`, e);
+    });
 }
 
 async function upload () {
@@ -217,7 +263,15 @@ async function upload () {
       spinner.start();
     }
 
-    await uploadAllFiles(src, version, dst);
+    let files;
+
+    if (Array.isArray(src)) {
+      files = parseFiles(src);
+    } else {
+      files = await getAllFiles(src);
+    }
+
+    await uploadAllFiles(files, version, dst);
 
     if (VERBOSE) {
       spinner.stop();
@@ -225,12 +279,6 @@ async function upload () {
   }
 
   log(`Done`);
-}
-
-// For debugging
-async function listFiles (prefix = 'airship-components-test/') {
-  const files = await listObjects({Prefix: prefix, Bucket: secrets.AWS_S3_BUCKET });
-  log(`${files.Contents.length} with prefix '${prefix}'`)
 }
 
 upload();
