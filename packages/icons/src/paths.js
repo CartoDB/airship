00e7c19b4bea405f6f9fce7fca8ec9e490403d6c
Román Jiménez <roman@cartodb.com>
2018-12-18 20:01:33 +0100

    Add comment explaining why the paths are not on airship-icons

--- packages/icons/src/paths.js
+++ packages/icons/src/paths.js
@@ -1,1 +1,5 @@
+// This has been moved to components because I need the paths to dynamically create SVGs using JSX
+// and Typescript does not let you import files from outside the project root.
+// TODO: Since we're using lerna, we could move it back here, define icons as a dependency to component and
+// lerna would symlink them locally.
 module.exports = require('../../components/src/resources/icon-paths.json');
