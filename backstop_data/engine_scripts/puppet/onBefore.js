e9ce6ad8d64c179f324f73922fc7dc31d96ed431
Román Jiménez <roman@cartodb.com>
2019-06-25 20:41:05 +0200

    wait for 250 ms on onBefore

--- backstop_data/engine_scripts/puppet/onBefore.js
+++ backstop_data/engine_scripts/puppet/onBefore.js
@@ -2,7 +2,9 @@ module.exports = async (page, scenario, vp) => {
   page.on('load', function () {
     page.evaluate(() => {
       document.fonts.ready.then(() => {
-        console.log('_READY');
+        setTimeout(() => {
+          console.log('_READY');
+        }, 250);
       });
     });
   });
