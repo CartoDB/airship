590e38bdc3636912153effd1a75145dd5f74305f
elenatorro <elenatorro@gmail.com>
2019-06-28 15:58:10 +0200

    Add specs, snapshots and e2e tests

--- packages/components/src/components.d.ts
+++ packages/components/src/components.d.ts
@@ -235,7 +235,7 @@ export namespace Components {
     /**
     * Returns the current selection
     */
-    'getSelection': () => Promise<string[] | number[]>;
+    'getSelection': () => Promise<number[] | string[]>;
     /**
     * Title of the widget to be displayed
     */
