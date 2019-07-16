faa7a398f46433f5dd797ef631878ae67ebdac6c
elenatorro <elenatorro@gmail.com>
2019-07-16 09:05:54 +0200

    Use only date time by default

--- packages/components/src/components/common/constants.ts
+++ packages/components/src/components/common/constants.ts
@@ -5,5 +5,5 @@ export const DEFAULT_BACKGROUND_BAR_COLOR_HEX = '#E2E6E3';
 export const DEFAULT_BAR_COLOR = `var(--as--color--complementary, ${DEFAULT_BAR_COLOR_HEX})`;
 export const DEFAULT_BACKGROUND_BAR_COLOR = `var(--as-color--type-03, ${DEFAULT_BACKGROUND_BAR_COLOR_HEX})`;
 export const DEFAULT_NUMBER_FORMAT = '%Q';
-export const DEFAULT_DATE_FORMAT = '%x - %X';
+export const DEFAULT_DATE_FORMAT = '%x';
 export const AUTO_FORMAT = 'auto';
