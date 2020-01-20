27d096188d8788482971483faf66c478199313b1
jesusbotella <jesus.botella@gmail.com>
2020-01-20 12:04:39 +0100

    Placeholder content

---
+++ packages/components/src/components/as-placeholder/placeholder-content/chart/as-placeholder-chart.tsx
@@ -0,0 +1,70 @@
+import { Component, Element, h, Prop, State, Listen } from '@stencil/core';
+
+@Component({
+  shadow: false,
+  styleUrl: './as-placeholder-chart.scss',
+  tag: 'as-placeholder-chart',
+})
+export class PlaceholderChart {
+  @Element() public element: HTMLElement;
+  @Prop() public barWidth: number = 10;
+  @State() public elementWidth: number;
+
+  public resizeTimerId;
+
+  @Listen('resize', { target: 'window', passive: true })
+  public onWindowResize() {
+    window.clearTimeout(this.resizeTimerId);
+    this.resizeTimerId = setTimeout(() => { this.setElementWidth(); }, 250);
+  }
+
+  public componentDidLoad() {
+    this.setElementWidth();
+  }
+
+  public render() {
+    const numberOfBars = calculateBarsQuantity(this.elementWidth, this.barWidth);
+    const placeholderBars = this.renderBars(numberOfBars);
+
+    return [
+      <section class='placeholder__vaxis'>
+        <div class='vaxis-item vaxis-item--y'></div>
+        <div class='vaxis-item vaxis-item--y'></div>
+        <div class='vaxis-item vaxis-item--y'></div>
+        <div class='vaxis-item vaxis-item--x'></div>
+      </section>,
+
+      <section class='placeholder-chart'>
+        {placeholderBars}
+      </section>
+    ];
+  }
+
+  public renderBars(numberOfBars: number) {
+    const bars = [];
+
+    for (let i = 0; i < numberOfBars; i++) {
+      const barPercentage = Math.max(Math.trunc(Math.random() * 100), 15);
+
+      bars.push(
+        <as-placeholder-bar class='placeholder-chart__bar' height={`${barPercentage}%`} width={`${this.barWidth}px`}>
+        </as-placeholder-bar>
+      );
+    }
+
+    return bars;
+  }
+
+  public setElementWidth() {
+    const boundingRect = this.element.getBoundingClientRect();
+    this.elementWidth = boundingRect.width;
+  }
+}
+
+function calculateBarsQuantity(elementWidth, barWidth) {
+  if (!elementWidth) {
+    return 1;
+  }
+
+  return Math.floor(elementWidth / barWidth);
+}
---
+++ packages/components/src/components/as-placeholder/placeholder-content/list/as-placeholder-list.tsx
@@ -0,0 +1,34 @@
+import { Component, h } from '@stencil/core';
+
+@Component({
+  shadow: false,
+  styleUrl: './as-placeholder-list.scss',
+  tag: 'as-placeholder-list',
+})
+export class LoadingBar {
+  public render() {
+    return [
+      this.renderBarSection(),
+      this.renderBarSection(),
+      this.renderBarSection(),
+      this.renderBarSection(),
+      this.renderBarSection()
+    ];
+  }
+
+  public renderBarSection() {
+    const barPercentage = Math.max(Math.trunc(Math.random() * 100), 40);
+
+    return (
+      <div class='placeholder-list__items'>
+        <as-placeholder-bar
+          class='placeholder-list__item placeholder-list__item--first'
+          height='12px'
+          width={`${barPercentage}%`}
+        ></as-placeholder-bar>
+
+        <as-placeholder-bar class='placeholder-list__item' height='4px' width='100%'></as-placeholder-bar>
+      </div>
+    );
+  }
+}
