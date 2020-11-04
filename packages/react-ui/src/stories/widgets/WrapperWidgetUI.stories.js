154cab2164fc088e51a26b54ad0bc7e89642fe0e
Raúl Yeguas <neokore@gmail.com>
2020-11-04 19:56:14 +0100

    Fixes and improvements

--- packages/react-ui/src/stories/widgets/WrapperWidgetUI.stories.js
+++ packages/react-ui/src/stories/widgets/WrapperWidgetUI.stories.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Typography } from '@material-ui/core';
+import ColorizeIcon from '@material-ui/icons/Colorize';
+import MenuIcon from '@material-ui/icons/Menu';
 import WrapperWidgetUI from '../../widgets/WrapperWidgetUI';
 
 export default {
@@ -18,19 +20,6 @@ export default {
   }
 };
 
-const Icon = () => (<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-<g fill="none" fill-rule="evenodd">
-    <g fill="#036FE2">
-        <g>
-            <g>
-                <g>
-                    <path d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12 6.477 2 12 2zm0 2c-4.418 0-8 3.582-8 8s3.582 8 8 8 8-3.582 8-8-3.582-8-8-8zm0 3c.6 0 3 5.139 3 6.667C15 15.507 13.657 17 12 17s-3-1.492-3-3.333C9 12.139 11.4 7 12 7z" transform="translate(-375 -443) translate(97 427) translate(272 10) translate(6 6)"/>
-                </g>
-            </g>
-        </g>
-    </g>
-</g>
-</svg>);
 const Template = (args) => <WrapperWidgetUI {...args}><Typography>Your Content</Typography></WrapperWidgetUI>;
 
 export const Default = Template.bind({});
@@ -39,9 +28,18 @@ Default.args = { title: 'Default wrapper' };
 export const OnlyTitle = Template.bind({});
 OnlyTitle.args = { title: 'Default wrapper' };
 
+export const Expandable = Template.bind({});
+Expandable.args = { title: 'Expandable', expandable: true };
+
+export const NotExpandable = Template.bind({});
+NotExpandable.args = { title: 'Not Expandable', expandable: false };
+
+export const Loading = Template.bind({});
+Loading.args = { title: 'Loading', loading: true };
+
 export const WithActions = Template.bind({});
 WithActions.args = { title: 'Wrapper with actions', actions: [
-  { id: 'a1', name: 'Autostyle', icon: (<Icon />), action: () => alert('Action!') }
+  { id: 'a1', name: 'Autostyle', icon: (<ColorizeIcon />), action: () => alert('Action!') }
 ] };
 
 export const WithOptions = Template.bind({});
@@ -54,10 +52,20 @@ export const WithActionsAndOptions = Template.bind({});
 WithActionsAndOptions.args = {
   title: 'Wrapper with actions and options',
   actions: [
-    { id: 'a1', name: 'Autostyle', icon: (<Icon />), action: () => alert('Action!') }
+    { id: 'a1', name: 'Autostyle', icon: (<ColorizeIcon />), action: () => alert('Action!') }
   ],
   options: [
     { id: 'o1', name: 'Option 1', action: () => alert('Option 1!') },
     { id: 'o2', name: 'Option 2', action: () => alert('Option 2!') },
   ]
+};
+
+export const WithOptionsAndCustomIcon = Template.bind({});
+WithOptionsAndCustomIcon.args = {
+  title: 'Wrapper with options and custom icon',
+  options: [
+    { id: 'o1', name: 'Option 1', action: () => alert('Option 1!') },
+    { id: 'o2', name: 'Option 2', action: () => alert('Option 2!') },
+  ],
+  optionsIcon: (<MenuIcon />)
 };
--- packages/react-ui/src/widgets/CategoryWidgetUI.js
+++ packages/react-ui/src/widgets/CategoryWidgetUI.js
@@ -4,21 +4,45 @@ import { Button, Grid, Typography, makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    ...theme.typography.caption,
+    ...theme.typography.body2,
   },
+
+  element: {
+    cursor: 'pointer',
+
+    '&$unselected': {
+      color: theme.palette.text.disabled,
+      
+      '& $progressbar div': {
+        backgroundColor: theme.palette.text.disabled
+      }
+    },
+
+    '&:hover $progressbar div': {
+      backgroundColor: theme.palette.secondary.dark
+    }
+  },
+
+  label: {
+    fontWeight: theme.typography.fontWeightMedium
+  },
+
   progressbar: {
-    height: '5px',
+    height: theme.spacing(0.5),
     width: '100%',
-    margin: theme.spacing(0.75, 0, 1, 0),
-    borderRadius: '4px',
-    backgroundColor: theme.palette.text.disabled,
+    margin: theme.spacing(0.5, 0, 1, 0),
+    borderRadius: theme.spacing(0.5),
+    backgroundColor: theme.palette.action.disabledBackground,
 
     '& div': {
       height: '100%',
-      borderRadius: '4px',
+      borderRadius: theme.spacing(0.5),
       backgroundColor: theme.palette.secondary.main,
+      transition: `background-color ${theme.transitions.easing.sharp} ${theme.transitions.duration.shortest}ms`,
     },
   },
+
+  unselected: {},
 
   optionsSelectedBar: {
     marginBottom: theme.spacing(1),
@@ -76,9 +100,16 @@ function CategoryWidgetUI(props) {
       {data.map((d, i) => {
         const value = formatter(d.value || 0);
         return (
-          <Grid container onClick={() => categoryClicked(d.category)} key={i}>
+          <Grid
+            container
+            onClick={() => categoryClicked(d.category)}
+            key={i}
+            className={
+              `${classes.element}
+               ${selectedCategories.length > 0 && selectedCategories.indexOf(d.category) === -1 ? classes.unselected : ''}`
+            }>
             <Grid container item direction='row' justify='space-between'>
-              <span>{labels[d.category] ? labels[d.category] : d.category}</span>
+              <span className={classes.label}>{labels[d.category] ? labels[d.category] : d.category}</span>
               { typeof value === 'object' && value !== null ? (
                 <span>
                   {value.unit}
@@ -104,12 +135,12 @@ CategoryWidgetUI.propTypes = {
   data: PropTypes.arrayOf(
     PropTypes.shape({
       category: PropTypes.string.isRequired,
-      value: PropTypes.number.isRequired
+      value: PropTypes.number
     })
   ).isRequired,
   formatter: PropTypes.func,
   labels: PropTypes.object,
-  selectedCategories: PropTypes.array.isRequired,
+  selectedCategories: PropTypes.array,
   onSelectedCategoriesChange: PropTypes.func,
 };
 
